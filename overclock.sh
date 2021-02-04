5a918165d7360c5076d2556d10a747c753b8e96c
yashspatel23 <yash.spatel23@gmail.com>
2021-02-04 00:45:09 -0600

    minor

--- overclock.sh
+++ overclock.sh
@@ -1,4 +1,5 @@
-nvidia-settings -c :0 -q gpus
-nvidia-smi -i 0 -pl 100
-nvidia-settings -c :0 -a '[gpu:0]/GPUMemoryTransferRateOffset[2]=1000'
-nvidia-settings -c :0 -a '[gpu:0]/GPUGraphicsClockOffset[2]=100'
+#!/bin/bash
+sudo nvidia-settings -c :0 -q gpus
+sudo nvidia-smi -i 0 -pl 150
+sudo nvidia-settings -c :0 -a '[gpu:0]/GPUMemoryTransferRateOffset[2]=2775'
+sudo nvidia-settings -c :0 -a '[gpu:0]/GPUGraphicsClockOffset[2]=-550'
