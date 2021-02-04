e5a7f5438687db3d95a26bb017241a79d0deb13c
yashspatel23 <yash.spatel23@gmail.com>
2021-02-03 23:25:13 -0600

    curl

--- overclock.sh
+++ overclock.sh
@@ -1,2 +1,4 @@
-sudo nvidia-settings -c :0 -a [gpu:0]/GPUMemoryTransferRateOffset[3]=1300
-sudo nvidia-settings -c :0 -a [gpu:0]/GPUGraphicsClockOffset[3]=-550
+nvidia-settings -c :0 -q gpus
+nvidia-smi -i 0 -pl 100
+nvidia-settings -c :0 -a '[gpu:0]/GPUMemoryTransferRateOffset[2]=1000'
+nvidia-settings -c :0 -a '[gpu:0]/GPUGraphicsClockOffset[2]=100'
